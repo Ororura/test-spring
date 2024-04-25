b22af1de06fe9c044dc536962d631fe6c2a302c1
Ororura <hitmanlol890@gmail.com>
2024-04-25 10:53:43 +0300

    fix DB connection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,24 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        try (var connection = DB.connect()) {
+        try (Connection connection = DB.connect()) {
             System.out.println("Connected to the PostgreSQL database.");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM accounts");
+            if(resultSet.next()) {
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getArray(1));
+            }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
