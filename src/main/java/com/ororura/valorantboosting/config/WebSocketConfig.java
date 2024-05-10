77e7a9a09109892b55697c62c2991c6550047e41
Ororura <hitmanlol890@gmail.com>
2024-05-10 10:42:49 +0300

    fix

--- src/main/java/com/ororura/valorantboosting/config/WebSocketConfig.java
+++ src/main/java/com/ororura/valorantboosting/config/WebSocketConfig.java
@@ -20,7 +20,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/test").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").
+                setAllowedOriginPatterns("*").
+                withSockJS();
     }
 
 }
